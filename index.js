55f2586949616214bb4bebad18ebb9ea754a068d
Tauqueer Khan <tauqueer.khan@gmail.com>
2020-11-25 21:32:50 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+console.log(`
+
+Tauqueer Khan - Software Engineer
+
+📚 Github: https://www.github.com/tkoriginal
+🐦 Twitter: https://www.twitter.com/tkoriginale
+🌎 Portfolio: https://www.tkhan.ca (not up yet)
+
+Bio: 
+  Software Engineer with experience building applications using web technologies.
+  Built production applications with
+    🖥 Electron/React for Desktop
+    🌎 HTML/CSS/JS w/ React and Redux, Express, Postgres for Web
+    📱 React Native for Mobile
+
+  Currently Learning: 
+    🖥 Node CLI - Automating tasks with NodeJS
+`)
